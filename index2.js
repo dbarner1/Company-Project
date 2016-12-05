55c4887def887ec5d5fde52403e535f8d5c542ea
Dave Barner <dbarner@gmail.com>
2016-12-04 19:17:56 -0500

    adding interactive video proto

---
+++ index2.js
@@ -0,0 +1,24 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: 'Xa0Q0J5tOP0',
+        playerVars: {
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
+
+$('#play').on('click', function () {
+    player.playVideo();
+});
+
+$('#pause').on('click', function () {
+    player.pauseVideo();
+});
